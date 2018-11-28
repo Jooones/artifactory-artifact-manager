89e76d90d12eb27173d442f2a1352b8abe917d85
Jooones <jonathanV@bitbucket.org>
2018-11-28 19:07:24 +0100

    project setup

---
+++ src/main/java/com/jooones/artifactory/artifact/manager/ArtifactManagerMain.java
@@ -0,0 +1,8 @@
+package com.jooones.artifactory.artifact.manager;
+
+public class ArtifactManagerMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
